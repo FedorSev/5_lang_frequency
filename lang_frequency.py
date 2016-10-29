ef93fe94f303a2e5ecc64603b113d79fbbc03fe7
fedor <tdgeccrew@gmail.com>
2016-10-30 02:30:36 +0300

    lang_frequency

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
+import collections
+import re
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath) or os.path.isdir(filepath):
+        return None
+
+    with open(filepath,'r') as text_file:
+        text = text_file.read()
+        return text
 
 
 def get_most_frequent_words(text):
-    pass
+    split_text = re.findall(r'\w+', text)
+    words = collections.Counter(split_text).most_common(10)
+    return words
 
 
 if __name__ == '__main__':
-    pass
+    while True:
+        filepath = input('Введите путь к файлу: ')
+        data = load_data(filepath)
+
+        if data != None:
+            print(get_most_frequent_words(data))
+            break
+
+        else:
+            print('Некорректный адрес файла, повторите ввод')
+
